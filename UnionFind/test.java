0806ee3d5a74d3d2f55d7f05fde430a15d9edb68
Renhua1991 <wongrh1991@gmail.com>
2014-12-04 02:11:40 -0500

    UnionFind

---
+++ UnionFind/test.java
@@ -0,0 +1,25 @@
+package linkedList;
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        List a = new ArrayList();
+        a.add("sw");
+        a.add("c");
+        a.add("a");
+        a.add("g");
+        a.add("u");
+        
+        List b = new LinkedList();
+        b.add("d");
+        b.add("wad");
+        
+        System.out.println(a.get(3));
+        System.out.println(b.get(1));
+        
+        
+    	
+    	
+    	
+    } // end main()
+} // end class LinkListApp
